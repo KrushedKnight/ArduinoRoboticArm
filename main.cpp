61ee63e1698640b01376cab81577191e527f2521
beast-machine-2 <krishivmanyam@gmail.com>
2025-08-01 17:57:19 -0700

    working communication

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 int openSerial(const std::string& device) {
     int fd = open(device.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
@@ -34,18 +35,85 @@ int openSerial(const std::string& device) {
     tty.c_oflag = 0;
     tty.c_lflag = 0;
 
+
+
     tcflush(fd, TCIFLUSH);
     if (tcsetattr(fd, TCSANOW, &tty) != 0) {
         std::cerr << "Error from tcsetattr\n";
         exit(1);
     }
+    int flags = TIOCM_DTR | TIOCM_RTS;
+    if (ioctl(fd, TIOCMBIC, &flags) != 0) {
+        std::cerr << "Warning: Could not clear DTR/RTS\n";
+    }
+
 
     return fd;
 }
 
+void writeCommand(int serialPort, std::string &data) {
+    int success = write(serialPort, data.c_str(), data.size());
+    tcdrain(serialPort);
+    std::cout << success << std::endl;
+    std::cout << data << std::endl;
+}
+
 
 int main() {
+    if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Error SDL_Init\n";
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Arduino Control",
+                                      SDL_WINDOWPOS_CENTERED,
+                                      SDL_WINDOWPOS_CENTERED,
+                                      640, 480,
+                                      SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Error creating SDL window: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+
     int serialPort = openSerial("/dev/ttyACM0");
-    write(serialPort, "Hello World!", 12);
-    
+    usleep(50000);
+
+    int sAngle = 45;
+    std::string data;
+
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            else if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_w) {
+                    sAngle++;
+                    data = "s" + std::to_string(sAngle) + "\n";
+                    writeCommand(serialPort, data);
+                }
+                if (event.key.keysym.sym == SDLK_s) {
+                    sAngle--;
+                    data = "s" + std::to_string(sAngle) + "\n";
+                    writeCommand(serialPort, data);
+                }
+                else if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    running = false;
+                }
+            }
+            else if (event.type == SDL_KEYUP) {
+                if (event.key.keysym.sym == SDLK_w) {
+                    std::cout << "W key released\n";
+                }
+            }
+        }
+        SDL_Delay(5);  // prevent busy-waiting
+    }
+
+    return 0;
 }
